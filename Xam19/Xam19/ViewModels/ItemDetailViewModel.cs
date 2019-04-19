54f1143ad5d5325acd9cdd308dd50b849f87a2cb
Kash <castillo.sergio@gmail.com>
2019-04-19 09:13:57 +0200

    Proyectos para comparación de tiempos de compilación y depuración 17 vs 19

---
+++ Xam19/Xam19/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xam19.Models;
+
+namespace Xam19.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ Xam19/Xam19/Views/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xam19.Views
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
