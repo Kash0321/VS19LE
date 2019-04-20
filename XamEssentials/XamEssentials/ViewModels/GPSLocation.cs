5714dcbc282913a5d44a4d7b069166d4ca222f39
Kash <castillo.sergio@gmail.com>
2019-04-20 16:59:47 +0200

    Essentials

---
+++ XamEssentials/XamEssentials/ViewModels/GPSLocation.cs
@@ -0,0 +1,38 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamEssentials.ViewModels
+{
+    public class GPSLocation : BindableBase
+    {
+        public GPSLocation()
+        {
+            longitude = "???";
+            latitude = "???";
+            altitude = "???";
+        }
+
+        string longitude;
+        public string Longitude
+        {
+            get { return longitude; }
+            set { SetProperty(ref longitude, value); }
+        }
+
+        string latitude;
+        public string Latitude
+        {
+            get { return latitude; }
+            set { SetProperty(ref latitude, value); }
+        }
+
+        string altitude;
+        public string Altitude
+        {
+            get { return altitude; }
+            set { SetProperty(ref altitude, value); }
+        }
+    }
+}
---
+++ XamEssentials/XamEssentials/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,104 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace XamEssentials.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public IPageDialogService PageDialogService { get; set; }
+
+        public MainPageViewModel(INavigationService navigationService, IPageDialogService dialogService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+            PageDialogService = dialogService;
+        }
+
+        bool IsLightOn { get; set; }
+
+        public GPSLocation GPSLocation { get; set; } = new GPSLocation();
+
+        DelegateCommand getLocationCommand;
+        public DelegateCommand GetLocationCommand =>
+            getLocationCommand ?? (getLocationCommand = new DelegateCommand(async () => await GetLocationAsync()));
+
+        async Task GetLocationAsync()
+        {
+            try
+            {
+                GPSLocation.Longitude = "Loading...";
+                GPSLocation.Latitude = "Loading...";
+                GPSLocation.Altitude = "Loading...";
+                var request = new GeolocationRequest(GeolocationAccuracy.Best);
+                var location = await Geolocation.GetLocationAsync(request);
+
+                if (location != null)
+                {
+                    GPSLocation.Longitude = location.Longitude.ToString("F4");
+                    GPSLocation.Latitude = location.Latitude.ToString("F4");
+                    GPSLocation.Altitude = location.Altitude.HasValue ? location.Altitude.Value.ToString("F4") : "???";
+                }
+                else
+                {
+                    GPSLocation.Longitude = "No location";
+                    GPSLocation.Latitude = "No location";
+                    GPSLocation.Altitude = "No location";
+                }
+            }
+            catch (Exception ex)
+            {
+                await PageDialogService.DisplayAlertAsync("Error", $"Unable to get location. {ex.Message}", "Cancel");
+            }
+        }
+
+        DelegateCommand toggleLightCommand;
+        public DelegateCommand ToggleLightCommand =>
+            toggleLightCommand ?? (toggleLightCommand = new DelegateCommand(async () => await ToggleLightAsync()));
+
+        async Task ToggleLightAsync()
+        {
+            try
+            {
+                if (IsLightOn)
+                {
+                    await Flashlight.TurnOffAsync();
+                    IsLightOn = false;
+                }
+                else
+                {
+                    await Flashlight.TurnOnAsync();
+                    IsLightOn = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                await PageDialogService.DisplayAlertAsync("Error", $"Unable to manage lights. {ex.Message}", "Cancel");
+            }
+        }
+
+        private DelegateCommand vibrateCommand;
+        public DelegateCommand VibrateCommand =>
+            vibrateCommand ?? (vibrateCommand = new DelegateCommand(Vibrate));
+
+        void Vibrate()
+        {
+            try
+            {
+                var duration = TimeSpan.FromSeconds(3);
+                Vibration.Vibrate(duration);
+            }
+            catch (Exception ex)
+            {
+                PageDialogService.DisplayAlertAsync("Error", $"Unable to get vibration. {ex.Message}", "Cancel");
+            }
+        }
+    }
+}
