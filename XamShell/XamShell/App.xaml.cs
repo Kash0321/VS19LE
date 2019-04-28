33b3754032f382c5302a0121e3fd0bd88babfc05
Kash <castillo.sergio@gmail.com>
2019-04-28 16:19:32 +0200

    Recreación XamShell

--- XamShell/XamShell/App.xaml.cs
+++ XamShell/XamShell/App.xaml.cs
@@ -8,12 +8,18 @@ namespace XamShell
 {
     public partial class App : Application
     {
+        //TODO: Replace with *.azurewebsites.net url after deploying backend to Azure
+        public static string AzureBackendUrl = "http://localhost:5000";
+        public static bool UseMockDataStore = true;
 
         public App()
         {
             InitializeComponent();
 
-            DependencyService.Register<MockDataStore>();
+            if (UseMockDataStore)
+                DependencyService.Register<MockDataStore>();
+            else
+                DependencyService.Register<AzureDataStore>();
             MainPage = new AppShell();
         }
 
